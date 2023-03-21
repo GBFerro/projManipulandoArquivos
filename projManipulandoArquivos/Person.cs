91947342037d20263de8aed9af489a2586ad6977
GBFerro <giovani.ferro@usp.br>
2023-03-21 13:43:44 -0300

    Add project files.

---
+++ projManipulandoArquivos/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projManipulandoArquivos
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public char Gender { get; set; }
+
+        public Person(string name, char gender)
+        {
+            (Name, Gender) = (name, gender);
+            //Name = name;
+            //Gender = gender;
+        }
+
+        public override string? ToString()
+        {
+            return $"Nome: {this.Name} | Gênero: {this.Gender}";
+        }
+    }
+}
---
+++ projManipulandoArquivos/Program.cs
@@ -0,0 +1,77 @@
+﻿using projManipulandoArquivos;
+
+string file;
+
+
+Person person1 = CreatePerson();
+Person person2 = CreatePerson();
+
+
+Person CreatePerson()
+{
+    Console.WriteLine("Informe o nome da pessoa: ");
+    var name = Console.ReadLine();
+    Console.WriteLine("Informe o genero da pessoa: ");
+    var gender = char.Parse(Console.ReadLine());
+
+    return new Person(name, gender);
+}
+
+WriteFile(person1);
+WriteFile(person2);
+Console.Clear();
+
+Console.WriteLine("Informe o nome do arquivo a ser aberto: ");
+file = Console.ReadLine();
+Console.WriteLine(ReadFile(file));
+
+string ReadFile(string file)
+{
+    StreamReader sr = new StreamReader(file);
+    string text = "";
+
+    try
+    {
+        text = sr.ReadToEnd();
+    }
+    catch (Exception)
+    {
+        throw;
+    }
+    finally
+    {
+        sr.Close();
+    }
+    return text;
+}
+
+void WriteFile(Person person)
+{
+    try
+    {
+        if (File.Exists("backup.txt"))
+        {
+            var temp = ReadFile("backup.txt");
+            StreamWriter sw = new("backup.txt");
+            sw.WriteLine(temp + person.ToString());
+            sw.Close();
+        }
+        else
+        {
+            StreamWriter sw = new StreamWriter("backup.txt");
+            sw.WriteLine(person.ToString());
+            sw.Close ();
+        }
+
+    }
+    catch (Exception e)
+    {
+        throw;
+    }
+    finally
+    {
+        Console.WriteLine("Registro gravado com sucesso");
+        Thread.Sleep(1000);
+    }
+}
+
